59e4a4d0dac22d0c591b1e4c60496bb9a88cfaa8
Pernille Krogstad <kroper16@student.westerdals.no>
2017-03-30 11:13:22 +0200

    Added new files about Campus to project

---
+++ php/campusInfo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Campus</title>
+
+<link rel="stylesheet" type="text/css" href="campusInfo.css">   
+
+</head>
+    
+<body>
+    
+     <?php 
+        require "header.php"; 
+        
+        ?>
+    
+    <div class = "container">
+        
+    <p class = "overskrift">Campuser</p>
+
+    
+    
+    
+    
+    
+    
+    
+    </div>
+
+</body>
+</html>
