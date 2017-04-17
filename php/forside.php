b7b426f870888354e70994ae4afa731611f595cf
hevaknt <tehkav15@student.westerdals.no>
2017-04-17 11:22:30 +0200

    Add files via upload

---
+++ php/forside.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+    
+    <link rel="stylesheet" type="text/css" href="../css/forside.css">
+    
+<title>Forside</title>    
+    
+</head>
+
+    
+    <body>
+        
+        <div class="bottomContainer"></div>
+        
+
+    
+    
+
+    
+    
+    
+</body>
+
+</html>
