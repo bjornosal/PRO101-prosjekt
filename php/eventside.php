4f8efebf4fbb3094c2d0a830f1467b2ecaa08594
thoringemm <myrtho16@student.westerdals.no>
2017-04-19 10:54:27 +0200

    gjorde endring på eventside

--- php/eventside.php
+++ php/eventside.php
@@ -1,131 +1,36 @@
-<?php
-$require
-?>
+
 
 <!doctype html>
 <html>
-    <head>
+	<head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
 
-        <title>Eventkalender</title>
-
-        <link rel="stylesheet" type="text/css" href="stil.css">
-
-
-    </head>
-    <body>
-        <?php require 'header.php'; 
-        ?>
-
-
-
-
-
-
-        <div class="container1"></div>
-        <div class="container2"></div>
-        <div class="container3"></div>
-        <div class="container4">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-
-        <div class="container5"></div>
-        <div class="container6"></div>
-        <div class="container7"></div>
-        <div class="container8">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-        <div class="container9"></div>
-        <div class="container10"></div>
-        <div class="container11"></div>
-        <div class="container12">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-
-        <div class="container13"></div>
-        <div class="container14"></div>
-        <div class="container15"></div>
-        <div class="container16">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-        <div class="container17"></div>
-        <div class="container18"></div>
-        <div class="container19"></div>
-        <div class="container20">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-
-        <div class="container21"></div>
-        <div class="container22"></div>
-        <div class="container23"></div>
-        <div class="container24">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-        <div class="container25"></div>
-        <div class="container26"></div>
-        <div class="container27"></div>
-        <div class="container28">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-
-        <div class="container29"></div>
-        <div class="container30"></div>
-        <div class="container31"></div>
-        <div class="container32">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-
-        <div class="container33"></div>
-        <div class="container34"></div>
-        <div class="container35"></div>
-        <div class="container36">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-
-        <div class="container37"></div>
-        <div class="container38"></div>
-        <div class="container39"></div>
-        <div class="container40">
-            <a href="https://www.google.no/">Bestill her</a>
-            <a href="https://www.facebook.com">Facebook</a>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </body>
+        <title>Event Kalender</title>
+        
+      <link rel="stylesheet" href="stil.css">
+        
+		
+	</head>
+		<body>
+	
+            
+            <div class="container1"></div>
+            <div class="container2"></div>
+            <div class="container3"></div>
+            <div class="container4">
+                <a href="https://www.google.no/">Bestil her</a>
+                <a href="https://www.facebook.com">Facebook</a>
+             </div>
+            
+            
+            
+           
+            
+            
+          
+      </body>
 </html>
