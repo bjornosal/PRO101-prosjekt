85a4285443d8aa0a5f52b97f67463f3dbf999ffa
thoringemm <myrtho16@student.westerdals.no>
2017-04-19 12:01:06 +0200

    endret stilcss og eventside

--- php/eventside.php
+++ php/eventside.php
@@ -1,5 +1,6 @@
 
 
+
 <!doctype html>
 <html>
 	<head>
@@ -20,13 +21,29 @@
             
             <div class="container1"></div>
             <div class="container2"></div>
-            <div class="container3"></div>
-            <div class="container4">
+            <div class="container3">
                 <a href="https://www.google.no/">Bestil her</a>
                 <a href="https://www.facebook.com">Facebook</a>
              </div>
             
-            
+           <div id="map"></div>
+    <script>
+      function initMap() {
+        var uluru = {lat: 59.916379, lng: 10.761465};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
+    </script>
+        
             
            
             
