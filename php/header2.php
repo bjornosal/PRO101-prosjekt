d0e1eb1905f5cefed8e66d041425d4896d72f387
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-04-20 14:57:42 +0200

    Finished up new header concept. Added js and menu dropdown.

--- php/header2.php
+++ php/header2.php
@@ -1,6 +1,7 @@
 <div class="header-container">
 
     <!--Container with hamburgermenu-->
+    <!--Disappears if viewport too small-->
     <div class="menu-container"> 
         <div class="menu-button">
             <div class="menu-line"></div>            
@@ -14,7 +15,30 @@
     <div class="logo-container">
         <div class="logo"><strong>WEVENT</strong></div>
         <div class="tag-line-container"><p>event<br>finder</p></div>
-
     </div>
-
 </div>
+<div class="menu-dropdown">
+    <a href="#">CAMPUS</a>
+    <a href="#">EVENTS</a>
+    <a href="#">OM OSS</a>                
+    <a href="#">KALENDER</a>
+</div>
+
+
+<script>
+    /**Make changes to js so that it animates the movement to be more clean. Also add gray overlay on rest of screen when dropdown is shown.*/
+    $(document).ready(function(){
+        $('.menu-button').click(function() {
+            $('.menu-dropdown').toggle();
+        });
+    });
+
+    $(document).mouseup(function (e) {
+        var container = $(".menu-dropdown");
+
+        if (!container.is(e.target) && container.has(e.target).length === 0) 
+        {
+            container.hide();
+        }
+    });
+</script>
