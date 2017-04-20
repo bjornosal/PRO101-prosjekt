8bd765e0b06896043e23cd13c94206aff67e5e19
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-04-20 14:39:34 +0200

    Created a seperate header to possibly use instead of original header.

---
+++ php/header2.php
@@ -0,0 +1,20 @@
+<div class="header-container">
+
+    <!--Container with hamburgermenu-->
+    <div class="menu-container"> 
+        <div class="menu-button">
+            <div class="menu-line"></div>            
+            <div class="filler menu-line "></div>            
+            <div class="menu-line"></div>            
+            <div class="filler menu-line"></div>            
+            <div class="menu-line"></div>
+        </div>
+    </div>
+
+    <div class="logo-container">
+        <div class="logo"><strong>WEVENT</strong></div>
+        <div class="tag-line-container"><p>event<br>finder</p></div>
+
+    </div>
+
+</div>
