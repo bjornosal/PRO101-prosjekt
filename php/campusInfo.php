36f744504c0df5273627bc3c45e5d4575f3a4e46
Pernille Krogstad <kroper16@student.westerdals.no>
2017-04-20 16:29:27 +0200

    Changed added to Campus and photos added

--- php/campusInfo.php
+++ php/campusInfo.php
@@ -6,12 +6,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
+    
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<title>Campus</title>
 
-<title>New HMTL document by NewJect</title>
-
-    <link rel="stylesheet" type = "text/css" href = "/css/campusInfo.css">
+    <link rel = "stylesheet" type = "text/css" href = "../css/campusInfo.css">
     
 </head>
+    
 <body> 
     
      <?php 
@@ -20,7 +22,7 @@
 
 <nav></nav>
 <div class = "photo sectOne"></div>
-<div class = "text"><h1>Fjerdingen</h1><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-tip hamburger biltong chicken. Hamburger pastrami strip steak andouille frankfurter. Burgdoggen shank turducken, frankfurter fatback meatball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p></div>
+<div class = "text"><h1>Fjerdingen</h1><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-HDHHDHDHDHDHDHD frankfurter. Burgdoggen shank turducken, frankfurter fatback meatball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p></div>
 <div class = "photo sectTwo"></div>
 <div class = "text"><h1>Vulkan</h1><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-tip hamburger biltong chicken. Hamburger pastrami strip steak andouille frankfurter. Burgdoggen shank turducken, frankfurter fatback meatball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p><div id = "mapVulkan"></div></div>
 <div class = "photo sectThree"></div>
