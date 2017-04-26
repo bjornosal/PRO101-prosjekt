80cdf359f92655b0ed797ff8857458d574af8dc2
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-04-26 19:54:03 +0200

    Added select with options as search.php as proposal instead of searchbar.php

--- php/search.php
+++ php/search.php
@@ -1,29 +1,69 @@
 <div class="search-container">
     <div class="search-box">
         <select name="campus" class="search-criteria">
-            <option selected disabled hidden><span>Campus</span></option>
-            <option value="volvo">FJERDINGEN</option>
-            <option value="saab">VULKAN</option>
-            <option value="fiat">BRENNERIVEIEN</option>
+            <option selected disabled hidden>CAMPUS</option>
+            <option value="fjerdingen">FJERDINGEN</option>
+            <option value="vulkan">VULKAN</option>
+            <option value="brenneriveien">BRENNERIVEIEN</option>
         </select>
+
+        <select name="category" class="search-criteria">
+            <option selected disabled hidden>KATEGORI</option>
+            <option value="fjerdingen">LEK</option>
+            <option value="vulkan">MORO</option>
+            <option value="brenneriveien">TING</option>
+        </select>
+
+        <select name="from-month" class="search-criteria">
+            <option selected disabled hidden>FRA MÅNED</option>
+            <option value="januar">JANUAR</option>
+            <option value="februar">FEBRUAR</option>
+            <option value="mars">MARS</option>
+            <option value="april">APRIL</option>
+            <option value="mai">MAI</option>
+            <option value="juni">JUNI</option>
+            <option value="juli">JULI</option>
+            <option value="august">AUGUST</option>
+            <option value="september">SEPTEMBER</option>
+            <option value="oktober">OKTOBER</option>
+            <option value="november">NOVEMBER</option>
+            <option value="desember">DESEMBER</option>
+        </select>
+
+        <select name="to-month" class="search-criteria">
+            <option selected disabled hidden>FRA MÅNED</option>
+            <option value="januar">JANUAR</option>
+            <option value="februar">FEBRUAR</option>
+            <option value="mars">MARS</option>
+            <option value="april">APRIL</option>
+            <option value="mai">MAI</option>
+            <option value="juni">JUNI</option>
+            <option value="juli">JULI</option>
+            <option value="august">AUGUST</option>
+            <option value="september">SEPTEMBER</option>
+            <option value="oktober">OKTOBER</option>
+            <option value="november">NOVEMBER</option>
+            <option value="desember">DESEMBER</option>
+        </select>
+        <div class="search-button">SØK</div>
         <!--<p class="campus">CAMPUS</p>
 <div class="campus-dropdown dropdown">
 <div class="campus-options campusline">FJERDINGEN</div>
 
 <div class="campus-options campusline">VULKAN</div>
 <div class="campus-options">BRENNERIVEIEN</div>-->
+        <!--
+<div class="search-criteria">
+<p class="category">KATEGORI</p>
+</div>
 
-        <div class="search-criteria">
-            <p class="category">KATEGORI</p>
-        </div>
+<div class="search-criteria">
+<p class="from-month">FRA MÅNED</p>
+</div>
 
-        <div class="search-criteria">
-            <p class="from-month">FRA MÅNED</p>
-        </div>
-
-        <div class="search-criteria">
-            <p class="to-month">TIL MÅNED</p>
-        </div>
-
+<div class="search-criteria">
+<p class="to-month">TIL MÅNED</p>
+</div>
+-->
     </div>
 </div>
