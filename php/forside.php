61575221ccac5d0e0d793db7d9cd9c0bdf5ab7f2
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-04-27 13:31:55 +0200

    Made minor changes to slideshow @ forside.php/css

--- php/forside.php
+++ php/forside.php
@@ -1,72 +1,84 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
-<meta name="author" content="">
-    
-    <link rel="stylesheet" type="text/css" href="../css/forside.css">
-    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    
-<title>Forside</title>
+        <link rel="stylesheet" type="text/css" href="../css/forside.css">
 
 
 
-</head>
+        <title>Forside</title>
 
-<body>
-    
-<?php 
+
+
+    </head>
+
+    <body>
+
+        <?php 
         require 'header.php'; 
-        
-        
-?>
+        require 'search.php';
+        require 'tip-box.php';
 
-<div class='slideshow'>
-    <div class='social-fb'>
-        <a href="https://www.facebook.com/sharer/sharer.php?u=">
-            <img src="../photos/facebook-icon.svg" alt="Facebook"> 
-        </a>
-        </div>
-    
-    <div class='social-tw'>
-        <a href="https://twitter.com/home?status=">
-            <img src="../photos/twitter-icon-square-logo.png" alt="Twitter"> 
-        </a>
-        </div>
-    
-    <div class='social-email'>
-        <a href="#">
-            <img src="../photos/email-icon.svg" alt="Email"> 
-        </a>
-    
-    </div>
-    
-    
-  <div class='slide'>
-    <div class='image01'>
-      <img src="../photos/we-event01.jpg">
-      </div>
-    <div class='info'> Fusce id gravida quam. Donec justo augue, dictum non luctus eget, eleifend eget justo. Nam interdum, ex ut vestibulum malesuada, lacus purus tincidunt eros, nec eleifend sapien leo sed leo. Suspendisse et mauris egestas tellus finibus laoreet non sed magna. Pellentesque porttitor elit vitae imperdiet pretium. Cras a accumsan purus, quis bibendum arcu. </div>
-    </div>
-    
-  <div class='slide'>
-    <div class='image02'>2</div>
-    <div class='info'> Halla </div>
-  </div>
-  <div class='slide'>
-    <div class='image03'>3</div>
-    <div class='info'> Yo </div>
-  </div>
-  
-  <div class='button back'>Back</div>
-  <div class='button forward'>Forward</div>
-  </div>
-    
 
-    
+        ?>
+        <!--
+
+TO-DO: 
+
+Ha framviseren til å være midstilt; ca 80% av skjermstørrelsen. 
+Du kan eventuelt ha faste størrelser på det. 
+
+
+
+
+-->
+        <div class='slideshow'>
+            <div class='social-fb'>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=">
+                    <img src="../photos/facebook-icon.svg" alt="Facebook"> 
+                </a>
+            </div>
+
+            <div class='social-tw'>
+                <a href="https://twitter.com/home?status=">
+                    <img src="../photos/twitter-icon-square-logo.png" alt="Twitter"> 
+                </a>
+            </div>
+
+            <div class='social-email'>
+                <a href="#">
+                    <img src="../photos/email-icon.svg" alt="Email"> 
+                </a>
+
+            </div>
+
+
+            <div class='slide'>
+                <div class='image01'>
+                    <img src="../photos/we-event01.jpg">
+                </div>
+                <div class='info'> Fusce id gravida quam. Donec justo augue, dictum non luctus eget, eleifend eget justo. Nam interdum, ex ut vestibulum malesuada, lacus purus tincidunt eros, nec eleifend sapien leo sed leo. Suspendisse et mauris egestas tellus finibus laoreet non sed magna. Pellentesque porttitor elit vitae imperdiet pretium. Cras a accumsan purus, quis bibendum arcu. </div>
+            </div>
+
+            <div class='slide'>
+                <div class='image02'>2</div>
+                <div class='info'> Halla </div>
+            </div>
+            <div class='slide'>
+                <div class='image03'>3</div>
+                <div class='info'> Yo </div>
+            </div>
+
+            <div class='button back'>Back</div>
+            <div class='button forward'>Forward</div>
+        </div>
+
+
+
     </body>
 </html>
