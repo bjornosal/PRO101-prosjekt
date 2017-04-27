4d3d4045efe293c315bad2ae9afc19fc9695d8b7
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-04-27 15:59:25 +0200

    New event-page in the works. Close to finished.

---
+++ php/event-calendar.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--Importing JQuery lib-->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+
+        <title>Eventfinder</title>
+
+        <link href="../css/style.css" rel="stylesheet" type="text/css">
+
+
+    </head>
+    <body>
+
+
+        <?php
+        require 'header.php';
+        require 'search.php';
+        require 'tip-box.php';
+        require 'go-to-top.php';
+        ?>
+
+        <div class="event-container">
+            <div class="event-background">
+                <div class="event-inner-container">
+                    <div class="event-photo-container">
+                        <img class="event-photo" src="../Bilder/Example_image.jpg">
+                    </div>
+                    <div id="ev1-map" class="event-map-container"></div>
+                    <div class="event-description-container">
+                        <h3 class="event-description-title">GENERISK TITTEL</h3>
+                        <p class="event-description-text">Generisk beskrivelse av et event.<br>Nisi occaecat iis efflorescere est eram expetendis non culpa dolore ita quem 
+                            quibusdam ne elit cillum nam laborum cohaerescant nam pariatur. Qui dolore 
+                            fidelissimae, id in imitarentur. Ex de consectetur, ea arbitror si laboris in 
+                            irure ingeniis in aliqua cillum, cernantur do quid. Senserit quis dolore do 
+                            veniam, incurreret si cernantur ut te minim laboris, ut dolore nisi non 
+                            arbitror, ut nisi se lorem ex multos proident quo comprehenderit, labore 
+                            sempiternum cernantur quorum iudicem sed ubi mentitum ne excepteur. Admodum anim 
+                            id cernantur graviterque, sed arbitror si nescius. Laborum varias laborum. Amet 
+                            graviterque eiusmod veniam proident e officia dolore fugiat hic elit o nescius 
+                            culpa excepteur eiusmod, pariatur elit fugiat pariatur illum, ingeniis malis 
+                            lorem est tamen a vidisse sint incididunt cernantur, qui ne fugiat sint labore 
+                            iis id enim malis qui singulis.</p>
+                    
+                    </div>
+                    <div class="event-social-container">
+                        <div class="event-facebook"></div>
+                        
+                    </div>
+                </div>
+                <div class="event-inner-container">
+                    <div class="event-photo-container"></div>
+                    <div id="ev1-map" class="event-map-container" ></div>
+                    <div class="event-description-container"></div>
+                    <div class="event-social-container"></div>
+                </div>
+            </div>
+        </div>
+
+        <!--Script needs major changes to support being different for each event from DB-->
+        <script>
+            function initMap() {
+                var uluru = {lat: 59.916379, lng: 10.761465};
+                var map = new google.maps.Map(document.getElementById('ev1-map'), {
+                    zoom: 14,
+                    center: uluru
+                });
+                var marker = new google.maps.Marker({
+                    position: uluru,
+                    map: map
+                });
+            }
+        </script>
+        <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRfr51h9jrxDI5yfrgQlqNMT6ySvGV6ek&callback=initMap">
+        </script>
+
+
+
+
+
+
+    </body>
+</html>
--- php/index.php
+++ php/index.php
@@ -1,3 +1,6 @@
+<?php
+    require 'db-login.php';
+?>
 
 <!DOCTYPE html>
 <html lang="no">
