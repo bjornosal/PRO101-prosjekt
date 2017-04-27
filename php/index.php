556bf51e5568b93167bc02127874382ddafe1769
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-04-27 13:22:22 +0200

    Made changes to about us style

--- php/index.php
+++ php/index.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="no">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
         <link href="../css/style.css" rel="stylesheet" type="text/css">
+        <link rel="icon" type="image/png" href="../photos/icons/cal-icon.png">
         <!--Importing JQUERY lib-->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <title>Wevent</title>
