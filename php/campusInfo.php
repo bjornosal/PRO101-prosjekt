c16d46e044f6ff7c2666e93d27e3c82432d0ff54
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2017-04-30 10:38:43 +0200

    Added php script to event-calendar.php. Now able to get events from the database.

--- php/campusInfo.php
+++ php/campusInfo.php
@@ -1,77 +1,77 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
-<meta name="author" content="">
-    
- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<title>Campus</title>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" type="image/png" href="../photos/icons/cal-icon.png">
 
-    <link rel = "stylesheet" type = "text/css" href = "../css/campusInfo.css">
-    
-</head>
-    
-<body> 
-    
-     <?php 
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <title>Campus</title>
+
+        <link rel = "stylesheet" type = "text/css" href = "../css/campusInfo.css">
+
+    </head>
+
+    <body> 
+
+        <?php 
         require 'header.php'; 
         require 'search.php';
         require 'tip-box.php';
         require 'go-to-top.php';
         ?>
-    
-    
-
-<nav></nav>
-<div class = "photo sectOne"></div>
-<div class = "text"><h1>Fjerdingen</h1><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-HDHHDHDHDHDHDHD frankfurter. Burgdoggen shank turducken, frankfurter fatback meaDDDDDDDDball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p><div class = "mapBox"></div><div id = "mapFjerdingen"></div></div>
-<div class = "photo sectTwo"></div>
-<div class = "text"><h1>Vulkan</h1><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-tip hamburger biltong chicken. Hamburger pastrami strip steak andouille frankfurter. Burgdoggen shank turducken, frankfurter fatback meatball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p><div id = "mapVulkan"></div></div>
-<div class = "photo sectThree"></div>
-<div class = "text"><h1>Brenneriveien</h1><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-tip hamburger biltong chicken. Hamburger pastrami strip steak andouille frankfurter. Burgdoggen shank turducken, frankfurter fatback meatball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p></div>
-
-    <script>
-    function initMap() {
-        var location = {lat: 59.923339, lng: 10.752497};
-        var mapVulkan = new google.maps.Map(document.getElementById("mapVulkan"), {
-            zoom: 4,
-            center: location
-        });
-        
-        var marker = new google.maps.Marker({
-            position: location,
-            map: mapVulkan
-        });
-    }
-    </script>
-    
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBdx92j0Ia-avzUehUghzLVIjeig8w8neE&callback=initMap"></script>
-    
-    
-    <script>
-    function initMap() {
-        var location = {lat: 59.916164, lng: 10.759675};
-        var mapFjerdingen = new google.maps.Map(document.getElementById("mapFjerdingen"), {
-            zoom: 4,
-            center: location
-        });
-        
-        var marker = new google.maps.Marker({
-            position: location,
-            map: mapFjerdingen
-        });
-    }
-    </script>
-    
-    <script async defer src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDbzIJIgHVAYw4oXACRP_XCt3Mnj_3T0dI&callback=initMap"></script>
 
 
-</body>
+
+        <nav></nav>
+        <div class = "photo sectOne"></div>
+        <div class = "text"><h1>Fjerdingen</h1><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-HDHHDHDHDHDHDHD frankfurter. Burgdoggen shank turducken, frankfurter fatback meaDDDDDDDDball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p><div class = "mapBox"></div><div id = "mapFjerdingen"></div></div>
+        <div class = "photo sectTwo"></div>
+        <div class = "text"><h1>Vulkan</h1><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-tip hamburger biltong chicken. Hamburger pastrami strip steak andouille frankfurter. Burgdoggen shank turducken, frankfurter fatback meatball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p><div id = "mapVulkan"></div></div>
+        <div class = "photo sectThree"></div>
+        <div class = "text"><h1>Brenneriveien</h1><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-tip hamburger biltong chicken. Hamburger pastrami strip steak andouille frankfurter. Burgdoggen shank turducken, frankfurter fatback meatball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p></div>
+
+        <script>
+            function initMap() {
+                var location = {lat: 59.923339, lng: 10.752497};
+                var mapVulkan = new google.maps.Map(document.getElementById("mapVulkan"), {
+                    zoom: 4,
+                    center: location
+                });
+
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map: mapVulkan
+                });
+            }
+        </script>
+
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBdx92j0Ia-avzUehUghzLVIjeig8w8neE&callback=initMap"></script>
+
+
+        <script>
+            function initMap() {
+                var location = {lat: 59.916164, lng: 10.759675};
+                var mapFjerdingen = new google.maps.Map(document.getElementById("mapFjerdingen"), {
+                    zoom: 4,
+                    center: location
+                });
+
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map: mapFjerdingen
+                });
+            }
+        </script>
+
+        <script async defer src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDbzIJIgHVAYw4oXACRP_XCt3Mnj_3T0dI&callback=initMap"></script>
+
+
+    </body>
 </html>
-    
-     
-    
-      
+
+
+
--- php/event-calendar.php
+++ php/event-calendar.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!--Importing JQuery lib-->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <link rel="icon" type="image/png" href="../photos/icons/cal-icon.png">
 
         <title>Eventfinder</title>
 
@@ -17,6 +18,7 @@
 
 
         <?php
+        require 'db-login.php';
         require 'header.php';
         require 'search.php';
         require 'tip-box.php';
@@ -25,26 +27,34 @@
 
         <div class="event-container">
             <div class="event-background">
+
+                <?php 
+                $statement = $connection -> prepare('SELECT * FROM event');
+
+                $statement -> execute();
+
+                while ($row = $statement -> fetch(PDO::FETCH_ASSOC)) {
+                    $events[] = $row;
+                }
+
+
+
+
+                /* echo '<pre>';
+                print_r($row);
+                die();*/
+
+                ?>
+                <?php foreach($events as $event) { ?>
                 <!--Start event showcase-->
                 <div class="event-inner-container">
                     <div class="event-photo-container">
-                        <img class="event-photo" src="../Bilder/Example_image.jpg">
+                        <img class="event-photo" src=$event['image_path'] alt="Bilde fra arrangement">
                     </div>
                     <div id="ev1-map" class="event-map-container"></div>
                     <div class="event-description-container">
-                        <h3 class="event-description-title">GENERISK TITTEL</h3>    
-                        <p class="event-description-text">Generisk beskrivelse av et event.<br>Nisi occaecat iis efflorescere est eram expetendis non culpa dolore ita quem 
-                            quibusdam ne elit cillum nam laborum cohaerescant nam pariatur. Qui dolore 
-                            fidelissimae, id in imitarentur. Ex de consectetur, ea arbitror si laboris in 
-                            irure ingeniis in aliqua cillum, cernantur do quid. Senserit quis dolore do 
-                            veniam, incurreret si cernantur ut te minim laboris, ut dolore nisi non 
-                            arbitror, ut nisi se lorem ex multos proident quo comprehenderit, labore 
-                            sempiternum cernantur quorum iudicem sed ubi mentitum ne excepteur. Admodum anim 
-                            id cernantur graviterque, sed arbitror si nescius. Laborum varias laborum. Amet 
-                            graviterque eiusmod veniam proident e officia dolore fugiat hic elit o nescius 
-                            culpa excepteur eiusmod, pariatur elit fugiat pariatur illum, ingeniis malis 
-                            lorem est tamen a vidisse sint incididunt cernantur, qui ne fugiat sint labore 
-                            iis id enim malis qui singulis.</p>
+                        <h3 class="event-description-title"><?= $event['title'] ?></h3>    
+                        <p class="event-description-text"><?= $event['description'] ?></p>
 
                     </div>
                     <div class="event-social-container">
@@ -64,89 +74,8 @@
                     </div>
                 </div>
                 <!--End event showcase-->
-                
-                <!--Start event showcase-->
-                <div class="event-inner-container">
-                    <div class="event-photo-container">
-                        <img class="event-photo" src="../Bilder/Example_image.jpg">
-                    </div>
-                    <div id="ev2-map" class="event-map-container"></div>
-                    <div class="event-description-container">
-                        <h3 class="event-description-title">GENERISK TITTEL</h3>    
-                        <p class="event-description-text">Generisk beskrivelse av et event.<br>Nisi occaecat iis efflorescere est eram expetendis non culpa dolore ita quem 
-                            quibusdam ne elit cillum nam laborum cohaerescant nam pariatur. Qui dolore 
-                            fidelissimae, id in imitarentur. Ex de consectetur, ea arbitror si laboris in 
-                            irure ingeniis in aliqua cillum, cernantur do quid. Senserit quis dolore do 
-                            veniam, incurreret si cernantur ut te minim laboris, ut dolore nisi non 
-                            arbitror, ut nisi se lorem ex multos proident quo comprehenderit, labore 
-                            sempiternum cernantur quorum iudicem sed ubi mentitum ne excepteur. Admodum anim 
-                            id cernantur graviterque, sed arbitror si nescius. Laborum varias laborum. Amet 
-                            graviterque eiusmod veniam proident e officia dolore fugiat hic elit o nescius 
-                            culpa excepteur eiusmod, pariatur elit fugiat pariatur illum, ingeniis malis 
-                            lorem est tamen a vidisse sint incididunt cernantur, qui ne fugiat sint labore 
-                            iis id enim malis qui singulis.</p>
+                <?php } ?>
 
-                    </div>
-                    <div class="event-social-container">
-                        <a href="#">
-                            <div class="copy-event-btn">
-                                <img class="event-btn-img" src="../photos/icons/copy-icon-black.png">
-                                <p><strong>KOPIER LENKE</strong></p>
-                            </div>
-                            <a href="#">
-                                <div class="ticket-event-btn">
-                                    <img class="event-btn-img" src="../photos/icons/ticket-icon-black.png">
-                                    <p><strong>BILLETTER</strong></p>
-
-                                </div>
-                            </a>
-                        </a>
-                    </div>
-                </div>
-                <!--End event showcase-->
-                
-                <!--Start event showcase-->
-                <div class="event-inner-container">
-                    <div class="event-photo-container">
-                        <img class="event-photo" src="../Bilder/Example_image.jpg">
-                    </div>
-                    <div id="ev3-map" class="event-map-container"></div>
-                    <div class="event-description-container">
-                        <h3 class="event-description-title">GENERISK TITTEL</h3>    
-                        <p class="event-description-text">Generisk beskrivelse av et event.<br>Nisi occaecat iis efflorescere est eram expetendis non culpa dolore ita quem 
-                            quibusdam ne elit cillum nam laborum cohaerescant nam pariatur. Qui dolore 
-                            fidelissimae, id in imitarentur. Ex de consectetur, ea arbitror si laboris in 
-                            irure ingeniis in aliqua cillum, cernantur do quid. Senserit quis dolore do 
-                            veniam, incurreret si cernantur ut te minim laboris, ut dolore nisi non 
-                            arbitror, ut nisi se lorem ex multos proident quo comprehenderit, labore 
-                            sempiternum cernantur quorum iudicem sed ubi mentitum ne excepteur. Admodum anim 
-                            id cernantur graviterque, sed arbitror si nescius. Laborum varias laborum. Amet 
-                            graviterque eiusmod veniam proident e officia dolore fugiat hic elit o nescius 
-                            culpa excepteur eiusmod, pariatur elit fugiat pariatur illum, ingeniis malis 
-                            lorem est tamen a vidisse sint incididunt cernantur, qui ne fugiat sint labore 
-                            iis id enim malis qui singulis.</p>
-
-                    </div>
-                    <div class="event-social-container">
-                        <a href="#">
-                            <div class="copy-event-btn">
-                                <img class="event-btn-img" src="../photos/icons/copy-icon-black.png">
-                                <p><strong>KOPIER LENKE</strong></p>
-                            </div>
-                            <a href="#">
-                                <div class="ticket-event-btn">
-                                    <img class="event-btn-img" src="../photos/icons/ticket-icon-black.png">
-                                    <p><strong>BILLETTER</strong></p>
-
-                                </div>
-                            </a>
-                        </a>
-                    </div>
-                </div>
-                <!--End event showcase-->
-                
-                
-                
             </div>
         </div>
 
