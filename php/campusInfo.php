333e82d38ab4ed442d4b3f05c14f7d2f9f895cb0
Pernille Krogstad <kroper16@student.westerdals.no>
2017-05-25 13:51:46 +0200

    Legger til info om campusene (in progress)

--- php/campusInfo.php
+++ php/campusInfo.php
@@ -40,26 +40,20 @@
         </div>
         
             
-            <div class = "text1"><h1>Fjerdingen</h1><div id = "mapFjerdingen"></div><p>Fjerdingen er Westerdals ACT sitt nye tilskudd til campus-familien, og stod ferdig til bruk høsten 2016. Navnet stammer fra gaten campusen ligger i, Christian Krohgs gate, som helt tilbake til 1600-tallet het Fjerdingens gate. Nå yrer gaten med studenter fra både kommunikasjon, teknologi og kunst. Mangfoldet av studenter fra maskedesign til programmering skaper et unikt miljø. Du sitter sjeldent alene i kantina på Fjerdingen. Fra torsdag-fredag er det dessuten også mulig å nyte en iskald pils i kantina som forvandles til Sjenkestua kl 17:00. Fjerdingen er dessuten også et steinkast unna Grünerløkka, Oslos svar på Soho i New York. Det skal ikke mer en enn liten spasertur ut av Fjerdingen for å utforske hva Oslo's hippeste strøk har å by på.</p></div>
+            <div class = "text1"><h1>Fjerdingen</h1><div id = "mapFjerdingen"></div><p>Fjerdingen er Westerdals ACT sitt nye tilskudd til campus-familien, og stod ferdig til bruk høsten 2016. Navnet stammer fra gaten campusen ligger i, Christian Krohgs gate, som helt tilbake til 1600-tallet het Fjerdingens gate. Nå fylles gaten med studenter fra alle avdelinger: Kommunikasjon, teknologi og kunst. Mangfoldet av studenter helt fra maskedesign til programmering skaper et unikt miljø. Fra torsdag-fredag er det dessuten også mulig å nyte en iskald pils i kantina som forvandles til Sjenkestua på kvelden. Fjerdingen ligger heller ikke langt unna Grünerløkka, Oslos svar på Soho i New York. Det skal ikke mer en enn liten spasertur ut av Fjerdingen for å utforske hva Oslo's hippeste strøk har å by på.</p></div>
         
         <div class = "photo sectTwo"></div>
         
         
      
-            <div class = "text2"><h1>Vulkan</h1><div id = "mapVulkan"></div><p>Vulkan er Fjerdingen's kule og litt eldre storesøster, og befinner seg 5-10 minutters gange unna. Miljøet rundt Vulkan er preget av </p></div>
+        <div class = "text2"><h1>Vulkan</h1><div id = "mapVulkan"></div><p>Fem til ti minutter unna Fjerdingen finner du Vulkan, Fjerdingen's storesøster. Her er campusen som tilbyr skuespillerlinje, film, TV og spill. I nærmiljøet rundt Vulkan finner du tilbud både for den matglade og kulturlystne, med Mathallen og konsertlokalet Vulkan Arena som naboer. Det finnes også anlegg for klatring og dans like ved, samt en rekke kafeér og restauranter. Rett utenfor finner du dessuten også Kuba-parken, en populær attraksjon for studentene om sommeren, med Fyrhuset Kuba i midten av parken. </p></div>
         
-    
-
-
 
 
         <div class = "photo sectThree"></div>
 
 
-        <div class = "text3"><h1>Brenneriveien</h1><div id = "mapBrenneriveien"></div><p>Bacon ipsum dolor amet spare ribs leberkas corned beef brisket tri-tip tail, kielbasa turkey jowl biltong salami filet mignon chicken. Leberkas shoulder short ribs ham, short loin frankfurter turkey landjaeger venison porchetta tri-tip hamburger biltong chicken. Hamburger pastrami strip steak andouille frankfurter. Burgdoggen shank turducken, frankfurter fatback meatball spare ribs t-bone. T-bone pig venison, ribeye spare ribs jerky cupim kielbasa sausage beef meatball. Meatball pastrami jowl, alcatra brisket sirloin tail short loin. Sirloin shoulder beef ribs chuck.</p></div>
-
-
-
+            <div class = "text3"><h1>Brenneriveien</h1><div id = "mapBrenneriveien"></div><p>Brenneriveien er Westerdals sin minste Campus, kun et steinkast unna Vulkan. På Brenneriveien hører studentene fra hår- og maskedesign til, samt enkelte deler av film, TV og spill avdelingen. Her finnes plattformene for de studentene som ønsker å uttrykke seg på en mer kreativ måte; både green-screen, verksted og klipperom er tilgjengelig for studentene. Dersom du skal være interessert i å utforske nærområdet rundt Brenneriveien, er kvelden tiden å utforske på. Det yrer av natteliv med gamle veteraner som Blå, og friske pust som Ingensteds. Om du er nysgjerrig på Oslo's gjemte perler finner du også disse i nærheten, som Bortenfor bak Ingensteds og Taxi Take Away på toppen av Brenneriveien.</p></div>
 
 
 
